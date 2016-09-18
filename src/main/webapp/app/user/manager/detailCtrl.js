c4f483e0244b7425e73fdbe6ddde7ce6e598d286
helei <emailtohl@163.com>
2016-09-18 12:20:35 +0800

    添加错误页

--- src/main/webapp/app/user/manager/detailCtrl.js
+++ src/main/webapp/app/user/manager/detailCtrl.js
@@ -15,5 +15,46 @@ define(['user/module', 'user/manager/service'], function(userModule) {
 			});
 		};
 		self.getDetail($state.params.id);
+		
+		self.dictionary = {
+			'ADMIN' : '系统管理员',
+			'EMPLOYEE' : '职员',
+			'MANAGER' : '经理',
+			'USER' : '普通用户',
+			'MALE' : '男',
+			'FEMALE' : '女',
+			'UNSPECIFIED' : '未知',
+		};
+		/**
+		 * 在详情中展示字符串，有的值是对象，所以需要处理
+		 */
+		self.getValue = function(k, v) {
+			var result, i;
+			switch (k) {
+				case 'department':
+					result = v.name;
+					break;
+				case 'subsidiary':
+					result = v.country + ' ' + v.province + ' ' + v.city;
+					break;
+				case 'iconSrc':
+					result = '';
+					break;
+				case 'gender':
+					result = self.dictionary[v];
+					break;
+				case 'authorities':
+					temp = [];
+					for (i = 0; i < v.length; i++) {
+						temp.push(self.dictionary[v[i]]);
+						result = temp.join('，');
+					}
+					break;
+				default:
+					result = v;
+				break;
+			}
+			return result;
+		};
 	}]);
 });
