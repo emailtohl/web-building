36362355f6f7ef67a12910649a0e24a776215264
helei <emailtohl@163.com>
2016-11-05 10:36:10 +0800

    添加系统信息监控

--- src/main/webapp/app/dashboard/dashboardCtrl.js
+++ src/main/webapp/app/dashboard/dashboardCtrl.js
@@ -63,17 +63,32 @@ define(['angular', 'dashboard/module', 'sparkline', 'knob'], function(angular) {
 					console.log('打开系统信息连接');
 				};
 				
+				var cpuPoints = [], $cpu = $('span#cpuInfo'), memoryPoints = [], $memory = $('span#memoryInfo'), swapPoints = [], $swap = $('span#swapInfo'), mpoints_max = 30;
 				connection.onmessage = function(e) {
 					if (!$state.includes('dashboard'))
 						return;
-					var data = JSON.parse(e.data), memory;
+					var data = JSON.parse(e.data);
 					if (data.getFreePhysicalMemorySize && data.getTotalPhysicalMemorySize) {
 						self.systemInfo.memory = (data.getFreePhysicalMemorySize / data.getTotalPhysicalMemorySize) * 100;
+						memoryPoints.push(self.systemInfo.memory);
+						if (memoryPoints.length > mpoints_max)
+							memoryPoints.splice(0, 1);
+						$memory.sparkline(memoryPoints);
 					}
 					if (data.getFreeSwapSpaceSize && data.getTotalSwapSpaceSize) {
 						self.systemInfo.swap = (data.getFreeSwapSpaceSize / data.getTotalSwapSpaceSize) * 100;
+						swapPoints.push(self.systemInfo.swap);
+						if (swapPoints.length > mpoints_max)
+							swapPoints.splice(0, 1);
+						$swap.sparkline(swapPoints);
 					}
-					self.systemInfo.cpu = data.getSystemCpuLoad;
+					if (data.getSystemCpuLoad) {
+						self.systemInfo.cpu = data.getSystemCpuLoad * 100;
+						cpuPoints.push(self.systemInfo.cpu);
+						if (cpuPoints.length > mpoints_max)
+							cpuPoints.splice(0, 1);
+						$cpu.sparkline(cpuPoints);
+					}
 					$scope.$apply();
 					$(".knob").knob().trigger('change');
 				};
@@ -86,6 +101,53 @@ define(['angular', 'dashboard/module', 'sparkline', 'knob'], function(angular) {
 			    	alert('WebSocketError! ' + e.data);
 			    }
 			})();
+			
+			
+			
+			
+			  /**
+			   ** Draw the little mouse speed animated graph
+			   ** This just attaches a handler to the mousemove event to see
+			   ** (roughly) how far the mouse has moved
+			   ** and then updates the display a couple of times a second via
+			   ** setTimeout()
+			   **/
+			  (function drawMouseSpeedDemo() {
+			    var mrefreshinterval = 500; // update display every 500ms
+			    var lastmousex = -1;
+			    var lastmousey = -1;
+			    var lastmousetime;
+			    var mousetravel = 0;
+			    var mpoints = [];
+			    var mpoints_max = 30;
+			    $('html').mousemove(function (e) {
+			      var mousex = e.pageX;
+			      var mousey = e.pageY;
+			      if (lastmousex > -1) {
+			        mousetravel += Math.max(Math.abs(mousex - lastmousex), Math.abs(mousey - lastmousey));
+			      }
+			      lastmousex = mousex;
+			      lastmousey = mousey;
+			    });
+			    var mdraw = function () {
+			      var md = new Date();
+			      var timenow = md.getTime();
+			      if (lastmousetime && lastmousetime != timenow) {
+			        var pps = Math.round(mousetravel / (timenow - lastmousetime) * 1000);
+			        mpoints.push(pps);
+			        if (mpoints.length > mpoints_max)
+			          mpoints.splice(0, 1);
+			        mousetravel = 0;
+			        $('#mousespeed').sparkline(mpoints, {width: mpoints.length * 2, tooltipSuffix: ' pixels per second'});
+			      }
+			      lastmousetime = timenow;
+			      setTimeout(mdraw, mrefreshinterval);
+			    };
+			    // We could use setInterval instead, but I prefer to do it this way
+			    setTimeout(mdraw, mrefreshinterval);
+			  })();
+			
+			
 		});
 	}]);
 });
