962acabfab36fc1d87ba5de2d60339d5128cceb9
helei <emailtohl@163.com>
2016-11-16 12:46:54 +0800

    调整了上传头像页面的显示样式

--- src/test/java/com/github/emailtohl/building/common/utils/UpDownloaderTest.java
+++ src/test/java/com/github/emailtohl/building/common/utils/UpDownloaderTest.java
@@ -7,7 +7,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
