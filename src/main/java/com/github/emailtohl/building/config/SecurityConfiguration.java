3bc9b1dbd23f52c8757adc7f984531b44dd196cd
helei <emailtohl@163.com>
2016-12-15 22:06:08 +0800

    集群环境的并发访问环境下，发现登录状态不稳定，先启用spring security的maxSessionsPreventsLogin特性

--- src/main/java/com/github/emailtohl/building/config/SecurityConfiguration.java
+++ src/main/java/com/github/emailtohl/building/config/SecurityConfiguration.java
@@ -190,7 +190,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				// 这允许Spring Security注册表建立一个会话它可以用来检测并发会话，见SecurityBootstrap.java
 				// maxSessionsPreventsLogin设置为true时，不允许用户在第二个地方同时登录，默认为false：如果用户在第二个地方登录则将前一会话置为失效
 				// 请注意,如果设置为true,没有明确的用户注销(例如刚刚关闭了浏览器)将无法再次登录,直到他们最初的会话到期
-					.sessionFixation().changeSessionId().maximumSessions(1)/*.maxSessionsPreventsLogin(true)*/
+					.sessionFixation().changeSessionId().maximumSessions(1).maxSessionsPreventsLogin(true)
 					.sessionRegistry(sessionRegistryImpl())
 				.and().and().csrf()/*.disable()*/.csrfTokenRepository(csrfTokenRepository())
 				.and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
