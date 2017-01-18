07c00acd1dd5a5b07eb6b8ce088099018ffc56d0
helei <emailtohl@163.com>
2017-01-18 11:49:02 +0800

    添加ztree组件

---
+++ src/main/java/com/github/emailtohl/building/common/ztree/ZtreeNode.java
@@ -0,0 +1,235 @@
+package com.github.emailtohl.building.common.ztree;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 前端zTree的数据模型
+ * 
+ * @author HeLei
+ */
+public class ZtreeNode implements Serializable , Comparable<ZtreeNode>{
+	private static final long serialVersionUID = -1932148922352477076L;
+	private transient static volatile int serial = 0;
+	
+	public ZtreeNode() {
+		synchronized(ZtreeNode.class) {
+			serial++;
+			id = serial;
+		}
+	}
+	
+	/**
+	 * 根据文件目录创建一个ZtreeNode实例
+	 * @param f 文件目录
+	 * @return ZtreeNode实例
+	 */
+	public static ZtreeNode newInstance(File f) {
+		return newInstance(f, 0);
+	}
+	
+	private static ZtreeNode newInstance(File f, int pid) {
+		ZtreeNode n = new ZtreeNode();
+		n.name = f.getName();
+		n.pid = pid;
+		if (f.isDirectory()) {
+			Set<ZtreeNode> children = new TreeSet<>();
+			for (File sf : f.listFiles()) {
+				children.add(newInstance(sf, n.id));
+			}
+			n.children = children;
+		}
+		return n;
+	}
+
+	
+	/** id 自动生成 */
+	private final int id;
+	
+	/** pid 父节点，根节点为0 */
+	private int pid = 0;
+	
+	/** 节点名 */
+	private String name;
+	
+	/** 节点的 checkBox / radio 的 勾选状态 [setting.check.enable = true & treeNode.nocheck = false 时有效] */
+	private boolean checked = false;
+	
+	/** 设置节点的 checkbox / radio 是否禁用 [setting.check.enable = true 时有效] */
+	private boolean chkDisabled = false;
+	
+	/** 设置节点是否隐藏 checkbox / radio [setting.check.enable = true 时有效] */
+	private boolean nocheck = false;
+	
+	/** 记录 treeNode 节点的 展开 / 折叠 状态 */
+	private boolean open = false;
+	
+	/** 节点自定义图标的 URL 路径 */
+	private String icon;
+	
+	/** 父节点自定义折叠时图标的 URL 路径 */
+	private String iconClose;
+	
+	/** 父节点自定义展开时图标的 URL 路径 */
+	private String iconOpen;
+	
+	/** 节点自定义图标的 className */
+	private String iconSkin;
+	
+	/** 设置点击节点后在何处打开 url。[treeNode.url 存在时有效] 例如：{ "id":1, "name":"test1", "url":"http://myTest.com", "target":"_blank"} */
+	private String target;
+	
+	/** 节点链接的目标 URL */
+	private String url;
+	
+	/** 节点的子节点数据集合， 在前端，如果是文件而非目录，该字段应该为null，所以此处不初始化 */
+	private Set<ZtreeNode> children;
+	
+	/** 记录 treeNode 节点是否为父节点 */
+	public boolean isParent() {
+		return children != null && !children.isEmpty();
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	public boolean isChkDisabled() {
+		return chkDisabled;
+	}
+
+	public void setChkDisabled(boolean chkDisabled) {
+		this.chkDisabled = chkDisabled;
+	}
+
+	public boolean isNocheck() {
+		return nocheck;
+	}
+
+	public void setNocheck(boolean nocheck) {
+		this.nocheck = nocheck;
+	}
+
+	public boolean isOpen() {
+		return open;
+	}
+
+	public void setOpen(boolean open) {
+		this.open = open;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getIconClose() {
+		return iconClose;
+	}
+
+	public void setIconClose(String iconClose) {
+		this.iconClose = iconClose;
+	}
+
+	public String getIconOpen() {
+		return iconOpen;
+	}
+
+	public void setIconOpen(String iconOpen) {
+		this.iconOpen = iconOpen;
+	}
+
+	public String getIconSkin() {
+		return iconSkin;
+	}
+
+	public void setIconSkin(String iconSkin) {
+		this.iconSkin = iconSkin;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Set<ZtreeNode> getChildren() {
+		return children;
+	}
+
+	public void setChildren(Set<ZtreeNode> children) {
+		this.children = children;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [id=" + id + ", name=" + name + ", children=" + children + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ZtreeNode other = (ZtreeNode) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(ZtreeNode o) {
+		return name.compareTo(o.name);
+	}
+	
+}
---
+++ src/test/java/com/github/emailtohl/building/common/ztree/ZtreeNodeTest.java
@@ -0,0 +1,55 @@
+package com.github.emailtohl.building.common.ztree;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.emailtohl.building.common.utils.UpDownloader;
+import com.google.gson.Gson;
+
+public class ZtreeNodeTest {
+	File test_root = new File("test_root");
+	File sub1 = new File(test_root, "sub1"),
+		 sub2 = new File(test_root, "sub2"),
+		 sub3 = new File(test_root, "sub3");
+	File sub1_1 = new File(sub1, "sub1_1"),
+		 sub1_2 = new File(sub1, "sub1_2");
+	File sub2_1 = new File(sub2, "sub2_1"),
+		 sub2_2 = new File(sub2, "sub2_2");
+	
+	@Before
+	public void setUp() throws Exception {
+		sub1.mkdirs();
+		sub2.mkdirs();
+		sub3.createNewFile();
+		sub1_1.createNewFile();
+		sub1_2.createNewFile();
+		sub2_1.createNewFile();
+		sub2_2.createNewFile();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		new UpDownloader("test_root").deleteDir(test_root.getAbsolutePath());
+	}
+
+	@Test
+	public void testNewInstance() {
+		ZtreeNode n = ZtreeNode.newInstance(test_root);
+		Gson gson = new Gson();
+		String json = gson.toJson(n);
+		System.out.println(json);
+		ZtreeNode nn = gson.fromJson(json, ZtreeNode.class);
+		System.out.println(nn);
+		assertEquals(nn, n);
+		int rootid = nn.getId();
+		int pid = nn.getChildren().iterator().next().getPid();
+		assertEquals(rootid, pid);
+		
+	}
+
+}
