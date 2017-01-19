6b064e02fe12f17ff0d4a23a292950a7965a7c74
helei <emailtohl@163.com>
2017-01-19 12:44:51 +0800

    TODO

--- src/main/webapp/app/cms/resource/ctrl.js
+++ src/main/webapp/app/cms/resource/ctrl.js
@@ -1,8 +1,8 @@
 define(['jquery', 'cms/module', 'cms/service', 'ztree'], function($, cmsModule) {
 	return cmsModule
-	.controller('ResourceCtrl', ['$scope', '$http', '$state', 'cmsService', 'ztreeutil',
-	                                function($scope, $http, $state, service, ztreeutil) {
-		var self = this, rootName = '', zTreeObj;
+	.controller('ResourceCtrl', ['$scope', '$http', '$state', 'cmsService', 'util', 'ztreeutil',
+	                                function($scope, $http, $state, service, util, ztreeutil) {
+		var self = this, style, zTreeObj;
 		var setting = {
 			edit : {
 				enable : true,
@@ -15,25 +15,45 @@ define(['jquery', 'cms/module', 'cms/service', 'ztree'], function($, cmsModule) 
 				beforeRemove : zTreeBeforeRemove,
 				beforeRename: zTreeBeforeRename,
 			},
+			view : {
+				selectedMulti: false,
+				addHoverDom : addHoverDom,
+			},
+			/*
+			check : {
+				enable: true,
+				chkStyle: 'checkbox',
+				chkboxType:  { 'Y' : 'ps', 'N' : 'ps' },
+			},
+			*/
 		};
+		util.loadasync('lib/ztree/zTreeStyle.css');
+		// 新增按钮的样式
+		style = $('<style type="text/css">'
+				+ '.ztree li span.button.add {margin-left:2px; margin-right: -1px; background-position:-144px 0; vertical-align:top; *vertical-align:middle}'
+				+ '</style>');
+		$('head').append(style);
 		$scope.getAuthentication();
 		getFileRoot();
 		
-		
 		function getFileRoot() {
 			service.getFileRoot().success(function(data) {
-				var zNodes = data.children;
-				rootName = data.name;
+				var zNodes = data;
+//				zNodes.name = 'root';
+				zNodes.open = true;
 				zTreeObj = $.fn.zTree.init($("#resource-tree"), setting, zNodes);
-				console.log(data);
 			});
 		}
 		
 		function zTreeBeforeRemove(treeId, treeNode) {
 			var filename;
+			if (treeNode.isFirstNode) {
+				alert('根目录不能删除!');
+				return false;
+			}
 			if (confirm('确认删除吗？')) {
 				filename = ztreeutil.getFilePath(treeNode);
-				service['delete'](rootName + '/' + filename).success(function(data) {
+				service['delete'](filename).success(function(data) {
 					getFileRoot();
 				});
 			}
@@ -43,15 +63,50 @@ define(['jquery', 'cms/module', 'cms/service', 'ztree'], function($, cmsModule) 
 		function zTreeBeforeRename(treeId, treeNode, newName, isCancel) {
 			var srcName, pre, destName;
 			if (!isCancel && newName.length > 0) {
-				srcName = rootName + '/' + ztreeutil.getFilePath(treeNode);
+				srcName = ztreeutil.getFilePath(treeNode);
 				pre = srcName.substring(0, srcName.lastIndexOf('/'));
-				destName = pre  + '/' + newName;
-				console.log(srcName)
-				console.log(destName)
+				if (pre) {
+					destName = pre  + '/' + newName;
+				} else {
+					destName = newName;
+				}
 //				service.reName(srcDir, newName);
 			}
 			return false;
 		}
+		
+		var newCount = 1;
+		function addHoverDom(treeId, treeNode) {
+			var sObj = $("#" + treeNode.tId + "_span");
+			if (treeNode.editNameFlag || $("#addBtn_" + treeNode.tId).length > 0) {
+				return;
+			}
+			if (!treeNode.isParent) {// 只在目录上新增目录节点，文件需要上传
+				return;
+			}
+			var addStr = "<span class='button add' id='addBtn_" + treeNode.tId
+				+ "' title='add node' onfocus='this.blur();'></span>";
+			sObj.after(addStr);
+			var btn = $("#addBtn_" + treeNode.tId);
+			if (btn) {
+				btn.bind("click", function() {
+					/*
+					var zTree = $.fn.zTree.getZTreeObj("resource-tree");
+					zTree.addNodes(treeNode, {
+						id : (100 + newCount),
+						pId : treeNode.id,
+						name : "new node" + (newCount++)
+					});
+					*/
+					var dirName = ztreeutil.getFilePath(treeNode);
+					dirName += '/new node' + (newCount++);
+					service.createDir(dirName).success(function(data) {
+						getFileRoot();
+					});
+					return false;
+				});
+			}
+		};
 	}])
 	;
 });
--- src/main/webapp/app/cms/service.js
+++ src/main/webapp/app/cms/service.js
@@ -12,14 +12,24 @@ define([ 'cms/module', 'common/context' ], function(cmsModule) {
 			 * 创建一个目录
 			 */
 			createDir : function(dirName) {
-				return $http.post('fileUploadServer/createDir', {dirName : dirName});
+				return $http({
+					method : 'POST',
+					url  : 'fileUploadServer/createDir',
+					data : 'dirName=' + encodeURIComponent(dirName),
+					headers : { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }
+				});
 			},
 			
 			/**
 			 * 为目录或文件改名
 			 */
 			reName : function(srcName, destName) {
-				return $http.post('fileUploadServer/reName', {srcName : srcName, destName : destName});
+				return $http({
+					method : 'POST',
+					url  : 'fileUploadServer/reName',
+					data : 'srcName=' + encodeURIComponent(srcName) + '&destName=' + destName,
+					headers : { 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8' }
+				});
 			},
 			
 			/**
