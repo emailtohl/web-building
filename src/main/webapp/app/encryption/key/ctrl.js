f67f598a2e655d6055e955c825ad9051c99d7c30
helei <emailtohl@163.com>
2017-01-25 10:40:45 +0800

    前端RSA加密模块

--- src/main/webapp/app/encryption/key/ctrl.js
+++ src/main/webapp/app/encryption/key/ctrl.js
@@ -3,19 +3,45 @@ define(['encryption/module', 'common/service/myrsa', 'encryption/service'], func
 	.controller('KeyCtrl', ['$scope', '$http', '$state', 'encryptionService', 'util',
 	                                function($scope, $http, $state, service, util) {
 		var self = this;
-		var keys = myrsa.generateKeys(512);
-		console.log(keys);
-		var plaintext = "滚滚长江东逝水，浪花淘尽英雄。\r\n" + 
-		"是非成败转头空。\r\n" + 
-		"青山依旧在，几度夕阳红。\r\n" + 
-		"白发渔樵江渚上，惯看秋月春风。\r\n" + 
-		"一壶浊酒喜相逢。\r\n" + 
-		"古今多少事，都付笑谈中。";
-		var c = myrsa.crypt(plaintext, keys);
-		var c_json = JSON.stringify(c);
-		console.log(c_json);
-		var r = myrsa.decrypt(JSON.parse(c_json), keys);
-		console.log(r);
+		self.bitLength = 1024;
+		if (localStorage.myrsakeys) {
+			self.isLocalStorage = true;
+			self.myrsakeys = localStorage.myrsakeys;
+		} else {
+			self.isLocalStorage = false;
+			self.myrsakeys = {};
+		}
+		
+		self.generateKeys = function() {
+			self.myrsakeys = myrsa.generateKeys(self.bitLength);
+			if (confirm('是否将密钥存入浏览本地缓存中？注意：请确保使用环境的安全！')) {
+				self.isLocalStorage = self.myrsakeys;
+			}
+		};
+		self.uploadPublicKey = function() {
+			
+		};
+		
+		
+		
+		test();
+		
+		
+		function test() {
+			var keys = myrsa.generateKeys(512);
+			console.log(keys);
+			var plaintext = "滚滚长江东逝水，浪花淘尽英雄。\r\n" + 
+			"是非成败转头空。\r\n" + 
+			"青山依旧在，几度夕阳红。\r\n" + 
+			"白发渔樵江渚上，惯看秋月春风。\r\n" + 
+			"一壶浊酒喜相逢。\r\n" + 
+			"古今多少事，都付笑谈中。";
+			var c = myrsa.crypt(plaintext, keys);
+			var c_json = JSON.stringify(c);
+			console.log(c_json);
+			var r = myrsa.decrypt(JSON.parse(c_json), keys);
+			console.log(r);
+		}
 		
 	}])
 	;
--- src/main/webapp/app/encryption/service.js
+++ src/main/webapp/app/encryption/service.js
@@ -1,16 +1,6 @@
-define(['encryption/module', 'common/context', 'lib/cryptico/cryptico.min' ], function(encryptionModule) {
+define(['encryption/module', 'common/context' ], function(encryptionModule) {
 	return encryptionModule.factory('encryptionService', [ '$http', 'util', function($http, util) {
 		return {
-			/**
-			 * 获取RSA对象
-			 */
-			getRsa : function(bitLength) {
-				var passPhrase = Math.random().toString();
-				return cryptico.generateRSAKey(passPhrase, bitLength);
-			},
-			/**
-			 * 加密
-			 */
 			
 		};
 	}]);
