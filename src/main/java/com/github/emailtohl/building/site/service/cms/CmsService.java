cbd261c7a332ea7d650b984f22832c216ec75de7
helei <emailtohl@163.com>
2017-02-15 15:07:39 +0800

    更新

---
+++ src/main/java/com/github/emailtohl/building/site/service/cms/CmsService.java
@@ -0,0 +1,9 @@
+package com.github.emailtohl.building.site.service.cms;
+/**
+ * cms的服务层接口
+ * @author HeLei
+ * @data 2017.02.15
+ */
+public interface CmsService {
+
+}
---
+++ src/main/java/com/github/emailtohl/building/site/service/cms/CmsServiceImpl.java
@@ -0,0 +1,10 @@
+package com.github.emailtohl.building.site.service.cms;
+
+/**
+ * cms的服务层实现
+ * @author HeLei
+ * @data 2017.02.15
+ */
+public class CmsServiceImpl implements CmsService {
+
+}
